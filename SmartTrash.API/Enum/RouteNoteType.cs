022aecfbd1090e49912ea03ee8f8439762eb6c75
hieu311001 <minhhieusn2k1@gmail.com>
2023-05-07 20:30:49 +0700

    thêm clear trash

---
+++ SmartTrash.API/Enum/RouteNoteType.cs
@@ -0,0 +1,14 @@
+﻿namespace SmartTrash.API.Enum
+{
+    public enum RouteNodeType
+    {
+        /// <summary>
+        /// Đang chờ
+        /// </summary>
+        GarbageTruck = 1,
+        /// <summary>
+        /// Đang thu gom
+        /// </summary>
+        RecycleBin = 2,
+    }
+}
